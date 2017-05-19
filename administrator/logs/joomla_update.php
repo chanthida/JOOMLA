fc495d9806bb3083fdec6dff0dffacf1774c4541
Chanthida <chanthida.adulwittayakorn@gmail.com>
2017-05-19 09:11:16 +0700

    second  commit

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-05-18 06:15:38 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-05-18T06:15:38+00:00	INFO 172.18.0.1	update	Update started by user Super User (119). Old version is 3.7.0.
+2017-05-18T06:15:43+00:00	INFO 172.18.0.1	update	Downloading update file from http://joomla-official-downloads.s3.amazonaws.com/joomladownloads/joomla3/Joomla_3.7.1-Stable-Update_Package.zip?AWSAccessKeyId=AKIAIZ6S3Q3YQHG57ZRA&Expires=1495088212&Signature=uVMUguHtys1hMKi4XSubonydFhM%3D.
+2017-05-18T06:16:25+00:00	INFO 172.18.0.1	update	File Joomla_3.7.1-Stable-Update_Package.zip successfully downloaded.
+2017-05-18T06:16:25+00:00	INFO 172.18.0.1	update	Starting installation of new version.
+2017-05-18T06:16:26+00:00	INFO 172.18.0.1	update	Finalising installation.
+2017-05-18T06:16:27+00:00	INFO 172.18.0.1	update	Deleting removed files and folders.
+2017-05-18T06:16:31+00:00	INFO 172.18.0.1	update	Cleaning up after installation.
+2017-05-18T06:16:31+00:00	INFO 172.18.0.1	update	Update to version 3.7.1 is complete.
